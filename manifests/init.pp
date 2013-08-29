addffabdcfafd5027db716e6b5df180705504c5e
Matt Cottingham <matt.cottingham@gmail.com>
2013-08-29 16:57:00 +0100

    Add tarsnap deb, use it is as installation source

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,11 +1,9 @@
-class tarsnap($ensure = present, $key_file = '/etc/tarsnap.key') {
-  include apt
-
-  apt::ppa { "ppa:ironix/tarsnap": }
+class tarsnap($ensure = latest, $key_file = '/etc/tarsnap.key') {
 
   package { "tarsnap":
+    provider => "dpkg",
     ensure  => $ensure,
-    require => Apt::Ppa["ppa:ironix/tarsnap"],
+    source => "puppet:///modules/tarsnap/pkg/tarsnap_1.0.35_amd64.deb",
   }
 
   file { "/etc/tarsnap.conf":
