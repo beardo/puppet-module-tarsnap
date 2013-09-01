c162569f22e0c922edfefbf0065f3a32b03d6a12
Matt Cottingham <matt.cottingham@gmail.com>
2013-09-01 16:41:52 +0100

    Copy tarsnap deb to /tmp before installing

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,13 +1,23 @@
 class tarsnap($ensure = latest, $key_file = '/etc/tarsnap.key') {
 
+  file { "/tmp/tarsnap_1.0.35_amd64.deb":
+    owner   => root,
+    group   => root,
+    mode    => 644,
+    ensure  => present,
+    source  => "puppet:///modules/tarsnap/pkg/tarsnap_1.0.35_amd64.deb",
+  }
+
   package { "tarsnap":
     provider => "dpkg",
     ensure  => $ensure,
-    source => "puppet:///modules/tarsnap/pkg/tarsnap_1.0.35_amd64.deb",
+    source => "/tmp/tarsnap_1.0.35_amd64.deb",
+    require => File['/tmp/tarsnap_1.0.35_amd64.deb'],
   }
 
   file { "/etc/tarsnap.conf":
     content => template("tarsnap/tarsnap.conf.erb"),
     require => Package["tarsnap"],
   }
+
 }
